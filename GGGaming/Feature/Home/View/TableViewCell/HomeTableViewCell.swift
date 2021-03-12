399fe065ac4506f0ace1bd4ff4ba8dc4645bf9e1
Philip Indra Prayitno <philip.prayitno@binus.edu>
2021-03-12 15:21:39 +0700

    feat: completing top rated games

--- GGGaming/Feature/Home/View/TableViewCell/HomeTableViewCell.swift
+++ GGGaming/Feature/Home/View/TableViewCell/HomeTableViewCell.swift
@@ -13,9 +13,20 @@ class HomeTableViewCell: UITableViewCell {
     @IBOutlet weak var labelGameRating: UILabel!
     @IBOutlet weak var labelGameReleaseDate: UILabel!
     @IBOutlet weak var imageGame: UIImageView!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var middleView: UIView!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.headerView.roundCorners(corners: [.topLeft, .topRight], radius: 6)
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        self.setupView()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -29,9 +40,15 @@ class HomeTableViewCell: UITableViewCell {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         guard let date = dateFormatter.date(from: data.gameReleaseDate ?? "") else { return }
-        dateFormatter.dateFormat = "dd MM yyyy"
+        dateFormatter.dateFormat = "dd MMMM yyyy"
         self.labelGameReleaseDate.text = dateFormatter.string(from: date)
         
         self.imageGame.sd_setImage(with: URL(string: data.gameImagePreview ?? ""), placeholderImage: #imageLiteral(resourceName: "icn-broken-photo"))
+    }
+    
+    private func setupView() {
+        self.containerView.layer.borderWidth = 1
+        self.containerView.layer.borderColor = UIColor.white.cgColor
+        self.containerView.layer.cornerRadius = 6
     }
 }
